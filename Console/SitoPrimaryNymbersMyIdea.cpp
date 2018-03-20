c6fb97bff54d0f150e7586fd9e7b5f5b30fa523d
Paweł Kolak <kolakpk@gmail.com>
2018-03-20 15:29:13 +0100

    fix

--- Console/SitoPrimaryNymbersMyIdea.cpp
+++ Console/SitoPrimaryNymbersMyIdea.cpp
@@ -20,13 +20,14 @@ void SitoPrimaryNymbersMyIdea::Run()
 	till = floor(sqrt(topLimit));	// pod³oga z pierwiasta, mo¿na to by³o zrobiæ te¿ np tak: (int)sqrt(topLimit) czyli powiedzenie mu ¿e wynik w fload chcemy interpretowaæ w int czyli z uwaleniem tego po przecinku
 
 	for (int i = 0; i <= topLimit - 1; i++)	// wype³nianie tablicy w ramach oszczêdnoœci zaczyna siê od 2 czyli tab[0]=2, tab[1]=3 ... itp
-		tab[i] = i+2;
+		tab[i] = i;
+	tab[1] = 0;
 
-	for (int i = 2; i < till; i++)	// Rdzeñ Sito czyli leci a¿ do pierwiastka z granicy
+	for (int i = 2; i <= till; i++)	// Rdzeñ Sito czyli leci a¿ do pierwiastka z granicy
 	{
 		if (tab[i] != 0)			// Leci po pozycjach i jeœli na któejœ jest 0 to pomija (jeœli tam jest 0 tzn ¿e ju¿ coœ z t¹ pozycj¹ zrobiliœmy (oznaczyliœmy jako wyzerowan¹))
 		{
-			for (int j = 2 * i; j <= topLimit - 1; j += i)	// Wypiepszamy wieloktotnoœci. Czyli bêd¹c na i(2) to wywalimy 2,4,6,8.... ³adny rysunek do tego jest tu http://www.algorytm.org/algorytmy-arytmetyczne/sito-eratostenesa.html
+			for (int j = (2 * i); j <= topLimit - 1; j += i)	// Wypiepszamy wieloktotnoœci. Czyli bêd¹c na i(2) to wywalimy 2,4,6,8.... ³adny rysunek do tego jest tu http://www.algorytm.org/algorytmy-arytmetyczne/sito-eratostenesa.html
 				tab[j] = 0;
 		}
 	}
